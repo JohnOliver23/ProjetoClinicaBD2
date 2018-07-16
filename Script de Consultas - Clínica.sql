b5eaffdcb6fe7a7a8d0ff5402293efde6fb8b95c
Pah <pah.dsf@gmail.com>
2018-07-16 10:46:14 -0300

    Querys aprimoradas 1 e 4 - Modificação query 7

--- Script de Consultas - Clínica.sql
+++ Script de Consultas - Clínica.sql
@@ -1,13 +1,20 @@
--- Consultas
+﻿-- Consultas
 
 -- 01 Exibir Pacientes e os nome dos médicos que os atenderam
 
+SELECT (select p.nome FROM Paciente p
+	WHERE p.codigo = c.codpac) Paciente,
+	(select f.nome FROM Funcionario f JOIN Medico m ON m.f_matricula = f.matricula
+	 WHERE m.f_matricula = c.matmed) Médico,
+	 c.data DataConsulta FROM Consulta c
+-- Consulta Otimizada
 SELECT p.nome Paciente , f.nome Médico, c.data DataConsulta 
 FROM consulta c JOIN funcionario f
 ON c.matmed = f.matricula
-JOIN paciente p ON c.codpac = p.codigo 
+JOIN paciente p ON c.codpac = p.codigo
 
 -- 02 Exibir Paciente com resultado e data da analise da radiografia
+
 SELECT p.nome Paciente , a.descricao Resultado, a.data DataAnalise FROM
 paciente p JOIN consulta c ON p.codigo = c.codpac
 JOIN radiografia r ON c.codigo = r.codconsulta 
@@ -19,6 +26,9 @@ SELECT nome, cnpj FROM convenio
 
 -- 04 Exibir nome dos pacientes e seus convênios
 
+select p.nome Paciente, (Select c.nome from Convenio c 
+				where p.convenio = c.cnpj) Convênio FROM paciente p
+-- Consulta Otimizada
 SELECT p.nome Paciente, c.nome Convênio
 FROM paciente p JOIN convenio c 
 ON p.convenio = c.cnpj
@@ -39,11 +49,12 @@ ON ra.matrad = r.f_matricula JOIN
 funcionario f ON r.f_matricula = f.matricula
 GROUP BY f.nome, r.crtr
 
--- 07 Exibir Radiografias com sua data e tipo
+-- 07 Exibir nome dos pacientes e o tipo e a data das Radiografias que fizeram
 
-SELECT t.descricao TipoRad, r.data
-FROM radiografia r JOIN tipo t ON
-r.codtipo = t.codigo
+SELECT p.nome Paciente, t.descricao TipoRad, r.data FROM radiografia r
+JOIN tipo t ON r.codtipo = t.codigo
+JOIN Consulta c ON c.codigo = r.codconsulta
+JOIN Paciente p ON p.codigo = c.codpac
 
 -- 08 Exibir resultados das Análises e médicos que realizaram.
 
